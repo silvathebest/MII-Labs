f7e6edcc17ddf2c889644d268ae580ec9425ea26
Alexey Romanov <51180322+silvathebest@users.noreply.github.com>
2021-10-04 20:26:38 +0400

    fix lab2

--- lab2.py
+++ lab2.py
@@ -1,5 +1,6 @@
 import pandas as pd
 import numpy as np
+from pandas import DataFrame
 
 pd.set_option('display.max_rows', 2000)
 pd.set_option('display.max_columns', 1000)
@@ -12,38 +13,39 @@ numberSeats = 'numberSeats'
 
 table = pd.read_csv('carsInfo.csv')
 
-default_maxSpeed = 180
-default_weight = 1000
-default_acceleration = 10.0
-default_numberSeats = 4
-min_maxSpeed = 50
-min_weight = 500
-max_acceleration = 2.0
-max_numberSeats = 8
+
+def getNormalizeArray(array):
+    startIndex = int(len(array) * 0.2)
+    endIndex = int(len(array) * 0.8)
+    newArray = []
+    for element in array:
+        if not np.isnan((float(element))):
+            newArray.append((float(element)))
+    newArray.sort()
+    result = newArray[startIndex:endIndex]
+    return result
+
 
 maxSpeed_table = table[maxSpeed]
 weight_table = table[weight]
 acceleration_table = table[acceleration]
 numberSeats_table = table[numberSeats]
-# Убираем пропуски
-maxSpeed_table.update(maxSpeed_table.replace(np.nan, default_maxSpeed))
-# Убираем шумы
-table.loc[(table[maxSpeed] < min_maxSpeed), maxSpeed] = default_maxSpeed
 
-weight_table.update(weight_table.replace(np.nan, default_weight))
-table.loc[(table[weight] < min_weight), weight] = default_weight
+maxSpeed_normalize = getNormalizeArray(maxSpeed_table)
+weight_normalize = getNormalizeArray(weight_table)
+acceleration_normalize = getNormalizeArray(acceleration_table)
+numberSeats_normalize = getNormalizeArray(numberSeats_table)
 
-acceleration_table.update(acceleration_table.replace(np.nan, default_acceleration))
-table.loc[(table[acceleration] < max_acceleration), acceleration] = default_acceleration
+table_normalize = DataFrame({maxSpeed: maxSpeed_normalize,
+                             weight: weight_normalize,
+                             acceleration: acceleration_normalize,
+                             numberSeats: numberSeats_normalize})
 
-numberSeats_table.update(numberSeats_table.replace(np.nan, default_numberSeats))
-table.loc[(table[numberSeats] > max_numberSeats), numberSeats] = default_numberSeats
-
-table.to_csv('newCarInfo.csv', index=False)
+table_normalize.to_csv('newCarInfo.csv', index=False)
 
 # минимальный вес автомобиля
 # самый быстрый разгон до 100км/ч
 # средняя макс скорость
-print('Минимальный вес автомобиля:', table[weight].min())
-print('Самый быстрый разгон до 100км/ч:', table[acceleration].min())
-print('Средняя макс скорость:', table[maxSpeed].mean())
+print('Минимальный вес автомобиля:', table_normalize[weight].min())
+print('Самый быстрый разгон до 100км/ч:', table_normalize[acceleration].min())
+print('Средняя макс скорость:', table_normalize[maxSpeed].mean())
